3c0f947bd3f67d894d9587274d6bee4f02750fb9
Dhanushi@27 <premalathadhanushiya27@gmail.com>
2026-03-28 22:39:38 +0530

    update

--- src/components/canvas/StarsCanvas.tsx
+++ src/components/canvas/StarsCanvas.tsx
@@ -5,9 +5,13 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props: any) => {
   const ref = useRef<any>();
-  const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
-  );
+  const [sphere] = useState(() => {
+    const positions = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+    for (let i = 0; i < positions.length; i++) {
+      if (isNaN(positions[i])) positions[i] = 0;
+    }
+    return positions;
+  });
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
